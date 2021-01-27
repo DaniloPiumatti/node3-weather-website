eaddcb8978f275f9a24575e0a58a58b2a1e3194f
d.piumatti <d.piumatti@libellula.eu>
2021-01-27 19:32:15 +0100

    Init commit

---
+++ src/utils/geocode.js
@@ -0,0 +1,25 @@
+const request = require('request')
+
+const geocode = (address, callback) => {
+    const url = 'https://api.mapbox.com/geocoding/v5/mapbox.places/' + encodeURIComponent(address) + '.json?access_token=pk.eyJ1IjoiZHBpdW1hdHRpIiwiYSI6ImNra2F6bTNibTE5NmwybmxuajU3dmgyODYifQ.z-O9tFxjPXy596ANwXpNdw&limit=1'
+
+    request({url, json: true}, (error, {body}) => {
+            if(error){
+                callback('Unable to connect to location service', undefined)
+            }
+            else if(body.features.length < 1){
+                callback('No matching result', undefined)
+            }
+            else {
+            const latitude = body.features[0].center[1]
+            const longitude = body.features[0].center[0]
+            callback(undefined, {
+                latitude,
+                longitude,
+                location: body.features[0].place_name
+            })
+            }
+        })
+}
+
+module.exports = geocode
